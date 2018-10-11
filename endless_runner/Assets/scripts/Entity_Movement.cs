b7468a5938f604afd77faefa86cf82690aa73d05
LAPTOP-KAE8TVNR\Gebruiker <thekid72gaming@gmail.com>
2018-10-11 09:46:10 +0200

    removed start function and added comments

--- endless_runner/Assets/scripts/Entity_Movement.cs
+++ endless_runner/Assets/scripts/Entity_Movement.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Entity_Movement : MonoBehaviour {
+
+    
     [SerializeField] private float speed = 8;
-	// Use this for initialization
-	void Start () {
-		
-	}
+
 	
-	// Update is called once per frame
 	void Update () {
+        // moves things to the left
         transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
 
         if (transform.position.x < -13) Destroy(gameObject);
